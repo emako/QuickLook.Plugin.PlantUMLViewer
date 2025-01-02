c316d06b042009be2a6d9e62e9b2018782d678ff
ema <mccoy39082@163.com>
2025-01-02 14:18:05 +0800

    restruct: new common

--- QuickLook.Plugin/QuickLook.Plugin.PlantUMLViewer/Scripts/pack-zip.ps1
+++
@@ -1,5 +0,0 @@
-Remove-Item ..\QuickLook.Plugin.PlantUMLViewer.qlplugin -ErrorAction SilentlyContinue
-
-$files = Get-ChildItem -Path ..\bin\Release\ -Exclude *.pdb,*.xml
-Compress-Archive $files ..\QuickLook.Plugin.PlantUMLViewer.zip
-Move-Item ..\QuickLook.Plugin.PlantUMLViewer.zip ..\QuickLook.Plugin.PlantUMLViewer.qlplugin
